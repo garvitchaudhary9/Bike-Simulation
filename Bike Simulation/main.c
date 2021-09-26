c6c479f82b7383d91eab939bc51f1aea7cb3ef16
Garvit Chaudhary <chaudharygarvit9@gmail.com>
2021-09-26 19:12:07 +0530

    Initial Commit

---
+++ Bike Simulation/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Bike Simulation
+//
+//  Created by Garvit Chaudhary on 26/09/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
